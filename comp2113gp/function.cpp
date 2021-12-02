d4b1a4126f2bd85a09e16bdca53e81f2718f87b7
Gucangqing <77569411+Gucangqing@users.noreply.github.com>
2021-12-02 18:57:39 +0800

    adding one function

--- comp2113gp/function.cpp
+++ comp2113gp/function.cpp
@@ -12,6 +12,11 @@ int roll(int n1, int n2){
 
 //function to check if the distributed skill points meet the requirements
 bool checkinitdist(int HP, int AP, int initpoints, bool &initdist){
-  if (HP >= 1 && HP <= 5 && AP >= 1 && AP <= 5) return true;
+  if (HP >= 1 && HP <= 5 && AP >= 1 && AP <= 5&& (HP+AP)==initpoints) return true;
   else return false;
+}
+//function to check if the extra points distribution works
+bool checkextradist(int HP,int AP, int extrapoints){
+    if(HP>=0&&HP<=2&&AP>=0&&AP<=2&&(HP+AP)==extrapoints) return true;
+    else return false;
 }
